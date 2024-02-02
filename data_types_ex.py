25dfd0ac7eed7846ce4cab3719a3887d5035085e
Heidi Negrete <heidi@negrete.dev>
2024-02-02 11:37:15 -0500

    add exercise answers for 'Data Types'

---
+++ data_types_ex.py
@@ -0,0 +1,26 @@
+'''
+1.
+    'True' - string
+    False - bool
+    (1,2,3) - tuple
+    1.5 - float
+    [1,2,3] - list
+    2 - int
+    range(5) - range
+    {1,2,3} - set
+    None - NoneType
+    {'foo': 'bar'} - dict
+'''
+# 2.
+names = ('Asta', 'Butterscotch', 'Pudding', 'Neptune', 'Darwin')
+print(type(names))
+
+# 3
+pets = {
+    'Asta': 'dog',
+    'Butterscotch': 'cat',
+    'Pudding': 'cat',
+    'Neptune': 'fish',
+    'Darwin': 'lizard',
+}
+print(type(pets))
