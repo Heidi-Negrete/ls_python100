e496aa2df9782f36b1e46f0bca72f914c8f68c3d
Heidi Negrete <heidi@negrete.dev>
2024-02-02 12:39:47 -0500

    Complete 'Basic Operations' exercises.

---
+++ basic_operations_ex.py
@@ -0,0 +1,33 @@
+# 1
+print('Heidi' + ' ' + 'Negrete')
+
+# 2
+number = 4936
+
+thousands = number // 1000
+number = number % 1000
+hundreds = number // 100
+number = number % 100
+tens = number // 10
+ones = number % 10
+
+print((thousands, hundreds, tens, ones))
+
+# 3
+# the code shown prints '510' to the console
+
+# 4
+print(int('5') + int('10'))
+
+# 5
+# yes
+
+# 6
+# false
+
+# 7
+# WRONG - it will coerce the str to type int and drop all precision after the decimal result = 3
+# Correct answer is it will raise an error. will not perform implicit type conversion during an explicit conversion
+
+# 8
+# true, because the first element of the string '12' is 1 and it is compared to '9'
