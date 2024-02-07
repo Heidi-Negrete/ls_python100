32a752de3ca5e76f4ec09e02adac2adce14a36d6
Heidi Negrete <heidi@negrete.dev>
2024-02-07 12:03:16 -0500

    add directory of additional exercises required by py100 curriculum

---
+++ external_ls_exercises/reading_docs_2/syntaxerror.py
@@ -0,0 +1,10 @@
+speed_limit = 60
+current_speed = 80
+
+if current_speed > speed_limit:
+    print('"People are so bad at driving cars that '
+          "computers don\'t have to be that good to be "
+          'much better." -- Marc Andreessen')
+
+
+# A syntax error indicates that parser encountered an issue with the form of your code, in this case a missing :
